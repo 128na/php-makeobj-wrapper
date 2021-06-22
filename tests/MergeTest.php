2b5281f2d0a1037f1dbfbc971922d497c9a16bb2
128na <simutrans.128na@gmail.com>
2021-06-22 19:35:51 +0900

    update

--- tests/MergeTest.php
+++ tests/MergeTest.php
@@ -8,7 +8,7 @@ class MergeTest extends TestCase
 {
     protected function tearDown(): void
     {
-        @unlink(__DIR__.'/example/merged.pak');
+        @unlink(__DIR__.'/example/mergetest.pak');
 
         parent::tearDown();
     }
@@ -17,13 +17,13 @@ class MergeTest extends TestCase
     {
         $dir = __DIR__.'/example';
         $pakFile = 'example1.pak example2.pak';
-        $pakFileLibrary = 'merged.pak';
+        $pakFileLibrary = 'mergetest.pak';
 
         $res = $this->getSUT()->merge($dir, $pakFileLibrary, $pakFile);
 
         $this->assertEquals(0, $res->getCode());
         $this->assertInstanceOf(MakeobjResponse::class, $res);
         $this->assertTrue(file_exists($dir.'/'.$pakFileLibrary));
-        $this->assertStringContainsString('merged.pak', $res->getStdOut());
+        $this->assertStringContainsString('mergetest.pak', $res->getStdOut());
     }
 }
--- tests/PakTest.php
+++ tests/PakTest.php
@@ -8,7 +8,7 @@ class PakTest extends TestCase
 {
     protected function tearDown(): void
     {
-        @unlink(__DIR__.'/example/testing.pak');
+        @unlink(__DIR__.'/example/paktest.pak');
 
         parent::tearDown();
     }
@@ -16,15 +16,15 @@ class PakTest extends TestCase
     public function test()
     {
         $dirpath = __DIR__.'/example';
-        $pakFile = 'testing.pak';
+        $pakFile = 'paktest.pak';
         $datFile = 'example.dat';
 
         $res = $this->getSUT()->pak($dirpath, 64, $pakFile, $datFile, true);
 
         $this->assertEquals(0, $res->getCode());
         $this->assertInstanceOf(MakeobjResponse::class, $res);
-        $this->assertTrue(file_exists($pakFile));
-        $this->assertStringContainsString('testing.pak', $res->getStdOut());
+        $this->assertTrue(file_exists($dirpath.'/'.$pakFile));
+        $this->assertStringContainsString('paktest.pak', $res->getStdOut());
         $this->assertStringContainsString('example.dat', $res->getStdOut());
         $this->assertStringContainsString('building.example1', $res->getStdOut());
         $this->assertStringContainsString('building.example2', $res->getStdOut());
